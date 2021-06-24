bf7982841dd703e715099f7ffdd687c6f04c5c29
Marcin <marcin.wrobel@hellwach-medien.de>
2021-06-24 13:20:38 +0200

    Basic Setup

---
+++ src/components/app/App.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react'
+import "./App.scss";
+
+export default function App(): ReactElement {
+    return (
+        <div className="App">
+            <h1>Hello</h1>
+        </div>
+    )
+}
